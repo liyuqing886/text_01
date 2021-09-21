3e33630ad8189e2a6f6a8682b118ccf66453d2d4
liyuqing <1923313276@qq.com>
2021-09-21 18:11:40 +0800

    first Commit

---
+++ text_01/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 李玉庆 on 2021/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
